8ca221e41e564e7e0f134f28c9b7c3cf0f8d30da
MirzaAyon <ayonmirza98@gmail.com>
2022-08-15 14:00:46 +0600

    header done with dropdown menu and bootstrap link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import logo from "../../images/logo.png";
 
 const Header = () => {
@@ -22,6 +22,17 @@ const Header = () => {
             <Nav.Link href="#features">Shop</Nav.Link>
             <Nav.Link href="#pricing">About</Nav.Link>
             <Nav.Link href="#review" >Review</Nav.Link>
+            <NavDropdown title="More" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Premium Books</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Choosed Books
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Information</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Promo
+              </NavDropdown.Item>
+            </NavDropdown>
           </Nav>
         </Container>
       </Navbar>
