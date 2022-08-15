32827b70475991c3c15f9db162974d8033940969
MirzaAyon <ayonmirza98@gmail.com>
2022-08-15 13:06:17 +0600

    website footer done with react bootstrap,accordian and 3 important questions

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,14 +3,14 @@ import { Accordion } from "react-bootstrap";
 
 const Footer = () => {
   return (
-    <div className="container my-5 shadow">
+    <div className="container my-5 ">
       <h1 className="text-center fw-bolder py-3">
-        Essential React.js Questions and Answers.
+        Important React.js Questions and Answers
       </h1>
       <Accordion defaultActiveKey="0" flush>
         <Accordion.Item eventKey="0">
           <Accordion.Header>How does React work?</Accordion.Header>
-          <Accordion.Body>
+          <Accordion.Body className="text-danger">
             React is a JavaScript library for building user interfaces. It is
             Component based declarative type which means we can re-use
             code.React creates a Virtual DOM in the memory Which is basically a
@@ -18,16 +18,18 @@ const Footer = () => {
             faster Than the Browser Dom.Instead of manipulating browsers Dom
             which is costly and not efficient, React creates Virtual DOM where
             it does all the manipulating before making change in the Browser
-            DOM. React’s Diffing Algorithm is used to compare the Virtual Dom
+            DOM. React’s Dif Algorithm is used to compare the Virtual Dom
             when something is changed, React goes to the actual DOM and updates
             the node that was updated in the virtual DOM.
+            <br></br>
+            <img className="h-50 w-50" src="https://www.tutorialswebsite.com/wp-content/uploads/2019/09/reactjs-component-lifecycle-methods.jpeg" alt="" />
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="1">
           <Accordion.Header>
             What is the difference between state and props in React?
           </Accordion.Header>
-          <Accordion.Body>
+          <Accordion.Body className="text-danger">
             Props are JavaScript Object which also known as properties.Props are
             used to pass data from one component to another componenet.Props can
             not be modified,read only and immutable. And State is also
@@ -35,12 +37,12 @@ const Footer = () => {
             only.States are used to manage the internal environment of a
             component means the data changes inside the component.State is both
             read and write and can be modified.State is local to the component
-            it can not be passed to other component.
+            it can not be passed to other component. State is used to set the data which are passed from useEffect.In react useState and useEffect are two important hooks
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="2">
           <Accordion.Header>how does React.useState work?</Accordion.Header>
-          <Accordion.Body>
+          <Accordion.Body className="text-danger">
             The useState() hook is a Special function which allow us to have
             state variable in functional component.We can pass the initial state
             it return us a variable with current state value and a fucntion
